f7daa9ede9fd1523d16f142f25ff109366178f35
정현기 <mejhg1234@gmail.com>
2020-06-11 19:59:50 +0900

    import LeanTween asset and add ui animation test codes

---
+++ Unity Projects/UI Animation Test/Assets/Scripts/LeanTweenBasics.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class LeanTweenBasics : MonoBehaviour
+{
+    public LeanTweenType easeType1;
+    public LeanTweenType easeType2;
+
+    public GameObject uiElement1;
+    public GameObject uiElement2;
+    public GameObject uiElement3;
+
+    public UnityEvent event1;
+    public UnityEvent event2;
+
+
+    void Update()
+    {
+
+        // scaling alternative ui panel
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            LeanTween.scale(uiElement1, Vector3.zero, 0.5f).setEase(easeType1);
+            LeanTween.scale(uiElement2, Vector3.one, 0.5f).setEase(easeType2);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            LeanTween.scale(uiElement1, Vector3.one, 0.5f).setEase(easeType2);
+            LeanTween.scale(uiElement2, Vector3.zero, 0.5f).setEase(easeType1);
+        }
+
+        // scaling child gameobjects consequently
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            event1.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            event2.Invoke();
+        }
+    }
+}
---
+++ Unity Projects/UI Animation Test/Assets/Scripts/LeanTweenSelfCallbacks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenSelfCallbacks : MonoBehaviour
+{
+    public void ScaleDownSelf()
+    {
+        LeanTween.scale(gameObject, Vector3.zero, 0.5f).setEaseInBack();
+    }
+
+    public void ScaleUpSelf()
+    {
+        LeanTween.scale(gameObject, Vector3.one, 0.5f).setEaseOutBack();
+    }
+}
