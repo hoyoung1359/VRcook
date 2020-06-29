89f57ed0b9504881a655593523bd5a189a290aa1
정현기 <mejhg1234@gmail.com>
2020-06-29 23:11:27 +0900

    Seperate timer parsing code into a function

--- Unity Projects/Speech Recognition Test/Assets/Scripts/CommandExecutor.cs
+++ Unity Projects/Speech Recognition Test/Assets/Scripts/CommandExecutor.cs
@@ -9,10 +9,12 @@ using UnityEngine.UI;
 /// </summary>
 [RequireComponent(typeof(VoiceRecognizer))]
 [RequireComponent(typeof(DatabaseRequest))]
+[RequireComponent(typeof(TimerManager))]
 public class CommandExecutor : MonoBehaviour
 {
     public Text text;
     private DatabaseRequest databaseRequest;
+    private TimerManager timerManager;
 
     void Start()
     {
@@ -20,6 +22,7 @@ public class CommandExecutor : MonoBehaviour
         recognizer.recognitionResultHandler += RecognitionResultHandler;
 
         databaseRequest = GetComponent<DatabaseRequest>();
+        timerManager = GetComponent<TimerManager>();
     }
 
     private void RecognitionResultHandler(object sender, string result)
@@ -29,50 +32,8 @@ public class CommandExecutor : MonoBehaviour
         
         if (result.StartsWith("타이머"))
         {
-            int time = 0;
             Debug.Log("Starting timer");
-            string timeresult = result.Substring(0, result.Length-1);
-            string[] timeList = timeresult.Split(' ');
-            for(int i=0; i<timeList.Length ; i++)
-            {
-                if (timeList[i].EndsWith("시간"))
-                {
-                    try
-                    {
-                        int hour = Int32.Parse(timeList[i].Substring(0, timeList[i].Length - 2));
-                        time += hour * 3600;
-                    }
-                    catch (Exception)
-                    {
-                        Debug.Log($"Unable to parse '{timeList[i]}'");
-                    }
-                }
-                else if (timeList[i].EndsWith("분"))
-                {
-                    try
-                    {
-                        int minute = Int32.Parse(timeList[i].Substring(0, timeList[i].Length - 1));
-                        time += minute * 60;
-                    }
-                    catch (Exception)
-                    {
-                        Debug.Log($"Unable to parse '{timeList[i]}'");
-                    }
-                }
-                else if (timeList[i].EndsWith("초"))
-                {
-                    try
-                    {
-                        int second = Int32.Parse(timeList[i].Substring(0, timeList[i].Length - 1));
-                        time += second;
-                    }
-                    catch (Exception)
-                    {
-                        Debug.Log($"Unable to parse '{timeList[i]}'");
-                    }
-                }
-            }
-            Debug.Log(time);
+            timerManager.StartTimer(ParseTime(result));
         }
 
         if (result.StartsWith("요청"))
@@ -93,5 +54,69 @@ public class CommandExecutor : MonoBehaviour
                 Debug.Log($"{column.name}:{column.value}");
             }
         }
+    }
+
+    // Calculates total duration of timer command in seconds
+    // The format for the command is "타이머 [x시간] [x분] [x초]."
+    // where brackets imply conditional arguments
+    // Any punctuation mark at the end is ignored
+    //
+    // Ex) "타이머 일분 30초." => 90
+    //     "타이머 1시간!" => 3600
+    private int ParseTime(string timerCommand)
+    {
+        var timeResult = timerCommand.Substring(0, timerCommand.Length - 1); // Remove punctuation mark
+        var timeList = timeResult.Split(' ');
+        var time = 0;
+
+        foreach (var token in timeList)
+        {
+            if (token.EndsWith("시간"))
+            {
+                var numericalPart = token.Substring(0, token.Length - 2);
+                try
+                {
+                    int hour = Int32.Parse(numericalPart);
+                    time += hour * 3600;
+                }
+                catch (Exception)
+                {
+                    // TODO: handle numbers in korean (e.g. 한시간)
+                    Debug.Log($"Unable to parse '{token}'");
+                }
+            }
+            else if (token.EndsWith("분"))
+            {
+                var numericalPart = token.Substring(0, token.Length - 1);
+                try
+                {
+                    int minute = Int32.Parse(numericalPart);
+                    time += minute * 60;
+                }
+                catch (Exception)
+                {
+                    // TODO: handle numbers in korean (e.g. 일분)
+                    Debug.Log($"Unable to parse '{token}'");
+                }
+            }
+            else if (token.EndsWith("초"))
+            {
+                var numericalPart = token.Substring(0, token.Length - 1);
+                try
+                {
+                    int second = Int32.Parse(numericalPart);
+                    time += second;
+                }
+                catch (Exception)
+                {
+                    // TODO: handle numbers in korean (e.g. 일초)
+                    Debug.Log($"Unable to parse '{token}'");
+                }
+            }
+        }
+
+        Debug.Log($"Parsed time in seconds: {time}");
+
+        return time;
     }
 }
