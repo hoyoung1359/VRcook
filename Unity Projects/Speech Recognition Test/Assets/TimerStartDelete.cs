4fbee0b92f1105ea7d0c070d97363ba8afa0d8ce
hoyoung1359 <63450118+hoyoung1359@users.noreply.github.com>
2020-07-14 06:21:25 +0900

    TimerStartDelete

---
+++ Unity Projects/Speech Recognition Test/Assets/TimerStartDelete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerStartDelete : MonoBehaviour
+{
+  
+    public void TimerStartButtonPressed()
+    {
+        
+    }
+
+    
+    public void TimerDeleteButtonPressed()
+    {
+        
+    }
+}
