f0a4d43e3111072657c537f449bcaa698abe6d1a
hoyoung1359 <63450118+hoyoung1359@users.noreply.github.com>
2020-07-27 00:16:30 +0900

    UI click

--- Unity Projects/Speech Recognition Test/Assets/Scripts/MenuID.cs
+++ Unity Projects/Speech Recognition Test/Assets/Scripts/MenuID.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MenuID : MonoBehaviour
 {
     public int id;
+
+    public void getRecipe()
+    {
+
+    }
 }
